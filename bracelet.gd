084a1b8c90007add63f22aa93d64d002019152ad
SeukWon Kang <kasworld@gmail.com>
2025-03-24 20:08:41 +0900

    add Bracelet

---
+++ bracelet.gd
@@ -0,0 +1,50 @@
+extends Node3D
+class_name Bracelet
+
+var orbitsphere_scene = preload("res://orbit_sphere/orbit_sphere.tscn")
+
+var orbit_r := 100.0
+var co1 := Color.MAGENTA
+var co2 := Color.YELLOW
+var visible_count :int = 3
+var count_inc :int = 1
+var orbsph_list :Array = []
+
+func init(n :int, ob_r :float) -> void:
+	orbit_r = ob_r
+	for i in n:
+		var sp_r = orbit_r / 50
+		var axis = Vector3.UP.rotated(Vector3.RIGHT.rotated(Vector3.UP,2*PI/n*i), PI/6 )
+		var orsp = orbitsphere_scene.instantiate(
+			).궤도설정(ob_r, 1, axis, 2*PI/n*i
+			).구설정(sp_r, 0, Vector3.UP
+		)
+		orbsph_list.append(orsp)
+		add_child(orsp)
+
+func change_궤도(n :int, orbit_r :float) -> void:
+	for o in orbsph_list:
+		o.hide()
+	for i in n:
+		var axis = Vector3.UP.rotated(Vector3.RIGHT.rotated(Vector3.UP,2*PI/n*i), PI/6 )
+		var rate := (i as float) / (n as float) *2
+		if rate > 1 :
+			rate = 2 - rate
+		orbsph_list[i].궤도설정(orbit_r, 1, axis, 2*PI/n*i
+			).구색설정(co2.lerp(co1, rate)
+			).궤도색설정(co1.lerp(co2, rate)
+		)
+		orbsph_list[i].show()
+
+func _process(delta: float) -> void:
+	change_visible_count()
+
+func change_visible_count() -> void:
+	visible_count += count_inc
+	if visible_count > orbsph_list.size():
+		count_inc = -1
+		visible_count = orbsph_list.size()
+	if visible_count < 3:
+		count_inc = 1
+		visible_count = 3
+	change_궤도(visible_count, orbit_r/2 *1.5)
--- main.gd
+++ main.gd
@@ -1,64 +1,13 @@
 extends Node3D
 
-var orbitsphere_scene = preload("res://orbit_sphere/orbit_sphere.tscn")
-
 var TotalDiagonal := 100.0
 var TotalHeight := 100
-var co1 := Color.YELLOW
-var co2 := Color.MAGENTA
 
 func _ready() -> void:
-	make_orbit_sphere(90, TotalDiagonal/2 *1.5)
-	visible_count = 3
-	count_inc = 1
-
-var visible_count :int
-var count_inc :int
-func change_visible_count() -> void:
-	visible_count += count_inc
-	if visible_count > orbsph_list.size():
-		count_inc = -1
-		visible_count = orbsph_list.size()
-	if visible_count < 3:
-		count_inc = 1
-		visible_count = 3
-	change_궤도(visible_count, TotalDiagonal/2 *1.5)
-
-var orbsph_list :Array = []
-func make_orbit_sphere(n :int, orbit_r :float) -> void:
-	for i in n:
-		var ob_r = orbit_r
-		var sp_r = orbit_r / 50
-		var axis = Vector3.UP.rotated(Vector3.RIGHT.rotated(Vector3.UP,2*PI/n*i), PI/6 )
-		var rate := (i as float) / (n as float)
-		var orsp = orbitsphere_scene.instantiate(
-			).궤도설정(ob_r, 1, axis, 2*PI/n*i
-			).구설정(sp_r, 0, Vector3.UP
-			).구색설정(co2.lerp(co1, rate)
-			).궤도색설정(co1.lerp(co2, rate)
-		)
-		orbsph_list.append(orsp)
-		add_child(orsp)
-
-func change_궤도(n :int, orbit_r :float) -> void:
-	for o in orbsph_list:
-		o.hide()
-	for i in n:
-		var axis = Vector3.UP.rotated(Vector3.RIGHT.rotated(Vector3.UP,2*PI/n*i), PI/6 )
-		var rate := (i as float) / (n as float)
-		orbsph_list[i].궤도설정(orbit_r, 1, axis, 2*PI/n*i
-			).구색설정(co2.lerp(co1, rate)
-			).궤도색설정(co1.lerp(co2, rate)
-		)
-		orbsph_list[i].show()
-
-func orbit_pos() -> void:
-	for n in orbsph_list:
-		n.position = Vector3.ZERO
+	$Bracelet.init(180, TotalDiagonal/2 *1.5)
 
 func _process(delta: float) -> void:
 	move_camera(delta)
-	change_visible_count()
 
 func move_camera(_delta: float) -> void:
 	var t = -Time.get_unix_time_from_system() /2.3
